1b14baa509546300c9b2d8029d8b74b21c382409
Vitor Mussatto <mussatto@gmail.com>
2015-09-20 09:25:10 -0300

    tests structure

--- routes/admin.js
+++ routes/admin.js
@@ -14,9 +14,8 @@ router.post('/create_post', function(req, res, next){
   console.log('post got');
   var data = req.body.short_tag;
   var posts = req.body.posts;
-  console.log('is this ok?');
-  console.log('data:'+data);
-  console.log('data:'+posts[0].title);
+
+  
 });
 
 module.exports = router;
--- test/user_test.js
+++ test/user_test.js
@@ -1,6 +1,13 @@
 var models = require("../models");
 
+var sync_test_case = function(){
+  models.sequelize.sync().then( function(){
+    console.log("Started!");
+  });
+};
 
-models.sequelize.sync().then( function(){
-  console.log("Started!");
-});
+var models_functions = function(){
+  it("should sync", sync_test_case);
+};
+
+describe("Models", models_functions);
